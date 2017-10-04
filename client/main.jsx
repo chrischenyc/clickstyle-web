661dccd3f82e4d768a31cea4148009877bd17611
Chris Chen <chrischen79@gmail.com>
2017-10-05 00:08:38 +1100

    add account-ui

--- client/main.jsx
+++ client/main.jsx
@@ -7,16 +7,22 @@ import Home from "../imports/ui/Home";
 import NewJob from "../imports/ui/NewJob";
 import EditJob from "../imports/ui/EditJob";
 import NotFound from "../imports/ui/NotFound";
+import Header from "../imports/ui/Header";
+import Footer from "../imports/ui/Footer";
 
 Meteor.startup(() => {
   render(
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/new-job" component={NewJob} /> // TODO: authenticate
-        <Route path="/edit-job" component={EditJob} /> // TODO: authenticate
-        <Route component={NotFound} />
-      </Switch>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/new-job" component={NewJob} /> // TODO: authenticate
+          <Route path="/edit-job" component={EditJob} /> // TODO: authenticate
+          <Route component={NotFound} />
+        </Switch>
+        <Footer />
+      </div>
     </Router>,
     document.getElementById("app")
   );
---
+++ imports/ui/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <p>HEADER</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
