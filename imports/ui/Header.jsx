e105af50348b383983086a26f3dda392854bace4
Chris Chen <chrischen79@gmail.com>
2017-10-05 00:32:01 +1100

    add meteor accounts-ui wrapper

--- imports/ui/Header.jsx
+++ imports/ui/Header.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import AccountsUIWrapper from "../ui/AccountsUIWrapper";
 
 class Header extends Component {
   render() {
     return (
       <div>
         <p>HEADER</p>
+        <AccountsUIWrapper />
       </div>
     );
   }
