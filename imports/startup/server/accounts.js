a9408a1d890a343b338e2a6b9291c2b57bfb3747
Chris Chen <chrischen79@gmail.com>
2017-10-12 12:32:07 +1100

    add default role 'customer' to newly created user

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -1,4 +1,6 @@
+import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
+import { Roles } from 'meteor/alanning:roles';
 import Profiles from '../../api/profiles/profiles';
 import normalizeProfile from '../../helpers/normalize-profile';
 
@@ -7,14 +9,18 @@ import normalizeProfile from '../../helpers/normalize-profile';
 Accounts.onCreateUser((options, user) => {
   const userToCreate = user;
 
-  // keep user profile in a separate collection
+  // keep a normalized user profile in a separate collection
   const normalizedProfile = normalizeProfile(options, user);
-
   if (normalizedProfile) {
+    // TODO: conditionally update existing user profile data fields
     Profiles.upsert({ userId: normalizedProfile.userId }, { $set: normalizedProfile });
   }
 
-  // TODO: set default user role to Customer
+  // TODO: set default user role as Customer
+  // https://stackoverflow.com/questions/22649600/unable-to-add-roles-to-user-with-meteor-using-roles-package/22650399#22650399s
+  Meteor.defer(() => {
+    Roles.addUsersToRoles(user._id, ['customer']);
+  });
 
   // TODO: send email verification for email/password registration
 
