96355c976e9f349594d0a030439daa9e98dcbb40
Chris Chen <chrischen79@gmail.com>
2017-10-16 22:13:00 +1100

    replace NavLink with Link in header as it's not working properly

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Container, Menu, Dropdown, Responsive } from 'semantic-ui-react';
 import { userSignedOut } from '../../modules/client/redux/user';
@@ -9,16 +9,16 @@ import { userSignedOut } from '../../modules/client/redux/user';
 const Header = props => (
   <Responsive as={Menu} minWidth={568} fixed="top" size="huge" inverted borderless>
     <Container>
-      <Menu.Item as={Link} to="/">
+      <Menu.Item as={Link} exact to="/">
         STYLESQUARD
       </Menu.Item>
       {props.authenticated ? (
         <Menu.Menu position="right">
           <Dropdown text="ACCOUNT" className="item">
             <Dropdown.Menu>
-              <Dropdown.Item as={NavLink} to="/dashboard" text="Dashboard" />
-              <Dropdown.Item as={NavLink} to="/profile" text="Profile" />
-              <Dropdown.Item as={NavLink} to="/settings" text="Settings" />
+              <Dropdown.Item as={Link} to="/dashboard" text="Dashboard" />
+              <Dropdown.Item as={Link} to="/profile" text="Profile" />
+              <Dropdown.Item as={Link} to="/settings" text="Settings" />
               <Dropdown.Item
                 text="Logout"
                 onClick={() => {
@@ -32,10 +32,10 @@ const Header = props => (
         </Menu.Menu>
       ) : (
         <Menu.Menu position="right">
-          <Menu.Item as={NavLink} to="/login">
+          <Menu.Item as={Link} to="/login">
             LOG IN
           </Menu.Item>
-          <Menu.Item as={NavLink} to="/signup">
+          <Menu.Item as={Link} to="/signup">
             SIGN UP
           </Menu.Item>
         </Menu.Menu>
