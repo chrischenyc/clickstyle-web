c5079b90b5d194d8efed0a646df1e4e92526405f
Chris Chen <chrischen79@gmail.com>
2017-10-17 23:07:40 +1100

    adjust user pages sidemenu layout

--- imports/ui/components/SideMenu.jsx
+++ imports/ui/components/SideMenu.jsx
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 
 const SideMenu = () => (
-  <Menu secondary vertical color="teal">
+  <Menu secondary vertical color="teal" size="massive" stackable compact>
     <Menu.Item name="Dashboard" as={NavLink} to="/dashboard" />
     <Menu.Item name="Profile" as={NavLink} to="/profile" />
-    <Menu.Item name="Bookings" as={NavLink} to="/#" disabled />
-    <Menu.Item name="Messages" as={NavLink} to="/#" disabled />
+    <Menu.Item name="Bookings" as={NavLink} to="/my-bookings" />
+    <Menu.Item name="Messages" as={NavLink} to="/messages" />
     <Menu.Item name="Settings" as={NavLink} to="/settings" />
   </Menu>
 );
