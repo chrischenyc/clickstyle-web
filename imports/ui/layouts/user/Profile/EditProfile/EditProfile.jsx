aafd878f9ffd29ff4b3e059d9d1028a17b83c3a8
Chris Chen <chrischen79@gmail.com>
2017-10-18 16:08:15 +1100

    build basic profile edit form

--- imports/ui/layouts/user/Profile/EditProfile/EditProfile.jsx
+++ imports/ui/layouts/user/Profile/EditProfile/EditProfile.jsx
@@ -1,10 +1,84 @@
+import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { userSignedIn } from '../../../../../modules/client/redux/user';
+import { validateUserLogin } from '../../../../../modules/validate';
 import EditProfilePage from './EditProfilePage';
 
+// platform-independent stateful container component
+// to handle edit user profile logic
 class EditProfile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      errors: {},
+      loading: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.setState({ errors: {} });
+    event.preventDefault();
+
+    const errors = validateUserLogin(this.state.email, this.state.password);
+
+    if (!_.isEmpty(errors)) {
+      this.setState({ errors });
+    } else {
+      this.setState({ loading: true });
+
+      // http://docs.meteor.com/api/accounts.html#Meteor-loginWithPassword
+      Meteor.loginWithPassword(this.state.email, this.state.password, (error) => {
+        if (error) {
+          this.setState({
+            loading: false,
+            errors: {
+              message: error.error === 403 ? 'email and password do not match' : error.reason,
+            },
+          });
+        } else {
+          this.setState({
+            loading: false,
+            errors: {},
+          });
+
+          this.props.userSignedIn(Meteor.user());
+        }
+      });
+    }
+  }
+
   render() {
-    return <EditProfilePage />;
+    return (
+      <EditProfilePage
+        onSubmit={this.handleSubmit}
+        onChange={this.handleChange}
+        loading={this.state.loading}
+        errors={this.state.errors}
+      />
+    );
   }
 }
 
-export default EditProfile;
+EditProfile.propTypes = {
+  userSignedIn: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => ({
+  userSignedIn: (user) => {
+    dispatch(userSignedIn(user));
+  },
+});
+export default connect(null, mapDispatchToProps)(EditProfile);
--- imports/ui/layouts/user/Profile/EditProfile/EditProfilePage.jsx
+++ imports/ui/layouts/user/Profile/EditProfile/EditProfilePage.jsx
@@ -1,5 +1,75 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Form, Message, TextArea } from 'semantic-ui-react';
+import _ from 'lodash';
 
-const EditProfilePage = () => <div>some forms</div>;
+import FormInputField from '../../../../components/FormInputField';
+
+// web version of the login form, stateless component
+const EditProfilePage = ({
+  onSubmit, onChange, loading, errors,
+}) => (
+  <Form onSubmit={onSubmit} loading={loading} error={!_.isEmpty(errors)}>
+    <FormInputField
+      label="First name"
+      placeholder="First name"
+      name="firstName"
+      onChange={onChange}
+      errors={errors}
+    />
+
+    <FormInputField
+      label="Last name"
+      placeholder="Last name"
+      name="lastName"
+      onChange={onChange}
+      errors={errors}
+      note={`This is not on your public profile. This is only shared with another ${Meteor.settings
+        .public.company.applicationName} user once you two have a confirmed booking .`}
+    />
+
+    <FormInputField
+      label="Phone number"
+      placeholder="Phone number"
+      name="phone"
+      onChange={onChange}
+      errors={errors}
+      note={`This is not on your public profile. This is only shared with another ${Meteor.settings
+        .public.company.applicationName} user once you two have a confirmed booking .`}
+    />
+
+    <FormInputField
+      label="Where about you"
+      placeholder="type to search ..."
+      name="addressSearch"
+      onChange={onChange}
+      errors={errors}
+      note={`This is not on your public profile. This is only shared with another ${Meteor.settings
+        .public.company.applicationName} user once you two have a confirmed booking .`}
+    />
+
+    <FormInputField
+      label="About you"
+      name="about"
+      control={TextArea}
+      onChange={onChange}
+      errors={errors}
+      note="Help other people get to know you. Tell them about the things you like..."
+    />
+
+    <Button color="teal" size="large" type="submit">
+      Save
+    </Button>
+
+    {!_.isEmpty(errors.message) && <Message error content={errors.message} />}
+  </Form>
+);
+
+EditProfilePage.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  errors: PropTypes.object.isRequired,
+};
 
 export default EditProfilePage;
