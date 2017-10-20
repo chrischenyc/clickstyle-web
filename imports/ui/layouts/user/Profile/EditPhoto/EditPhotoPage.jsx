e79ff55eaaf7b613b5d995c534c1730dc98fb8ce
Chris Chen <chrischen79@gmail.com>
2017-10-21 00:03:27 +1100

    update photo upload UI a bit

--- imports/ui/layouts/user/Profile/EditPhoto/EditPhotoPage.jsx
+++ imports/ui/layouts/user/Profile/EditPhoto/EditPhotoPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Message, Image, Progress, Grid, Button } from 'semantic-ui-react';
+import { Message, Image, Button } from 'semantic-ui-react';
 import _ from 'lodash';
 import { UploadField } from '@navjobs/upload';
 
@@ -9,48 +9,26 @@ const EditPhotoPage = ({
   photo, onFile, saving, errors,
 }) => (
   <div>
-    <Grid>
-      <Grid.Row>
-        <Grid.Column width="6">
-          <Image src={photo && photo.origin} size="medium" />
-        </Grid.Column>
+    <Image src={photo && photo.origin} size="medium" />
+    <p>Your photo will appear on your public profile</p>
+    <UploadField
+      onFiles={(files) => {
+        const file = files[0];
+        if (file) {
+          onFile(file);
+        }
+      }}
+      uploadProps={{
+        accept: '.jpg,.jpeg,.png,.bmp,.gif',
+      }}
+    >
+      <Button color="teal" loading={saving}>
+        Upload photo
+      </Button>
+      <span>&nbsp;image file max size: 2MB</span>
+    </UploadField>
 
-        <Grid.Column width="10">
-          <UploadField
-            onFiles={(files) => {
-              const file = files[0];
-              if (file) {
-                onFile(file);
-              }
-            }}
-            uploadProps={{
-              accept: '.jpg,.jpeg,.png,.bmp,.gif',
-            }}
-          >
-            <span>
-              Select your photo to upload (jpg, png, gif), it will appear on your public
-              profile&nbsp;
-            </span>
-            <Button color="teal" loading={saving}>
-              Select image
-            </Button>
-          </UploadField>
-
-          {!_.isEmpty(errors.message) && <Message error content={errors.message} />}
-          {saving && (
-            <Progress
-              percent={20}
-              autoSuccess
-              progress
-              indicating
-              color="teal"
-              size="small"
-              style={{ margin: '1rem 0' }}
-            />
-          )}
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+    {!_.isEmpty(errors.message) && <Message error content={errors.message} />}
   </div>
 );
 
