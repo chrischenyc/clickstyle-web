84ef65db99e2ea677960ed104c68b502232c4c37
Chris Chen <chrischen79@gmail.com>
2017-10-28 20:08:10 +1100

    add stylists join dummy form

--- imports/ui/layouts/stylists/StylistsJoin.jsx
+++ imports/ui/layouts/stylists/StylistsJoin.jsx
@@ -1,9 +1,85 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+
 import StylistsJoinPage from './StylistsJoinPage';
 
 class StylistJoin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      errors: {},
+      loading: false,
+      disabled: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.setState({ errors: {} });
+    event.preventDefault();
+
+    /*
+    const errors = validateUserSignUp(
+      this.state.email,
+      this.state.firstName,
+      this.state.lastName,
+      this.state.password,
+    );
+
+    if (!_.isEmpty(errors)) {
+      this.setState({ errors });
+    } else {
+      this.setState({ loading: true });
+
+      // http://docs.meteor.com/api/passwords.html#Accounts-createUser
+      Accounts.createUser(
+        {
+          email: this.state.email,
+          password: this.state.password,
+          profile: {
+            name: {
+              first: this.state.firstName,
+              last: this.state.lastName,
+            },
+          },
+        },
+        (error) => {
+          if (error) {
+            this.setState({
+              loading: false,
+              errors: {
+                message: error.reason,
+              },
+            });
+          } else {
+            this.setState({
+              loading: false,
+              errors: {},
+            });
+          }
+        },
+      );
+    }
+    */
+  }
+
   render() {
-    return <StylistsJoinPage />;
+    return (
+      <StylistsJoinPage
+        onSubmit={this.handleSubmit}
+        onChange={this.handleChange}
+        loading={this.state.loading}
+        errors={this.state.errors}
+        disabled={this.state.disabled}
+      />
+    );
   }
 }
 
--- imports/ui/layouts/stylists/StylistsJoinPage.jsx
+++ imports/ui/layouts/stylists/StylistsJoinPage.jsx
@@ -1,12 +1,61 @@
 import React from 'react';
-import { Container, Header, Button } from 'semantic-ui-react';
+import { Container, Header, Button, Divider, Form } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 
-const StylistsJoinPage = () => (
-  <Container style={{ padding: '8rem 0' }}>
-    <Header as="h1">Join the squad!</Header>
+import FormInputField from '../../components/FormInputField';
 
-    <Button content="Continue" size="huge" color="teal" icon="right arrow" labelPosition="right" />
+const StylistsJoinPage = ({
+  onSubmit, onChange, disabled, loading, errors,
+}) => (
+  <Container text style={{ padding: '8rem 0' }}>
+    <Header as="h1">Are you a stylist? Join the squad!</Header>
+
+    <p>
+      By joining stylesquad, you wil Expedita ducimus exercitationem ratione occaecati optio maxime
+      non. Non perferendis praesentium error et. Illum molestias quibusdam cumque eum neque.
+      Voluptas corporis fugiat tempora vitae animi dicta velit. Libero dignissimos dolore
+      consequatur in non nesciunt laborum. Laudantium modi ut optio pariatur necessitatibus sed
+      ullam itaque deserunt. Molestiae saepe accusantium ratione ut sunt doloribus non libero. Velit
+      explicabo eligendi possimus. Quia ab quibusdam. Nobis ducimus dolorem culpa deleniti commodi
+      velit voluptates. Est quia voluptates optio impedit. Eum ut est ut eligendi sed quis. Sapiente
+      dolor eius eos rem magni.
+    </p>
+
+    <Divider horizontal>tell us about you</Divider>
+
+    <Form onSubmit={onSubmit} loading={loading} error={!_.isEmpty(errors)}>
+      <FormInputField
+        fluid
+        placeholder="Field 1"
+        name="field1"
+        size="huge"
+        onChange={onChange}
+        errors={errors}
+      />
+
+      <FormInputField
+        fluid
+        placeholder="Field 2"
+        name="field2"
+        size="huge"
+        onChange={onChange}
+        errors={errors}
+      />
+
+      <Button color="teal" fluid size="huge" type="submit" disabled={disabled}>
+        Join the squad!
+      </Button>
+    </Form>
   </Container>
 );
+
+StylistsJoinPage.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  loading: PropTypes.bool.isRequired,
+  errors: PropTypes.object.isRequired,
+};
 
 export default StylistsJoinPage;
