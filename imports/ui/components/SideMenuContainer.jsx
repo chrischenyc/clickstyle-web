8322291155b067194102903e3b818f7fc2ef2f8c
Chris Chen <chrischen79@gmail.com>
2017-11-08 23:16:41 +1100

    hide side menu if on mobile

--- imports/ui/components/SideMenuContainer.jsx
+++ imports/ui/components/SideMenuContainer.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Responsive } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import SideMenu from './SideMenu';
 
 const SideMenuContainer = props => (
   <Grid stackable className="below-fixed-menu">
-    <Grid.Row>
-      <Grid.Column width="1">
-        <SideMenu />
-      </Grid.Column>
-      <Grid.Column width="15">{props.children}</Grid.Column>
-    </Grid.Row>
+    <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+      <Grid.Row>
+        <Grid.Column width="1">
+          <SideMenu />
+        </Grid.Column>
+        <Grid.Column width="15">{props.children}</Grid.Column>
+      </Grid.Row>
+    </Responsive>
+
+    <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>{props.children}</Responsive>
   </Grid>
 );
 
