690405a687d9a1451280cab34385d3006ed5bf6d
Chris Chen <chrischen79@gmail.com>
2017-11-15 12:12:10 +1100

    restrict stylist-only publication access

--- imports/api/stylists/server/publications.js
+++ imports/api/stylists/server/publications.js
@@ -1,21 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
+import { Roles } from 'meteor/alanning:roles';
 import Stylists from '../stylists';
 
 Meteor.publish('stylists.owner', function stylistsOwner() {
-  if (!this.userId) {
-    return null;
+  if (!Roles.userIsInRole(Meteor.userId(), [Meteor.settings.public.roles.stylist])) {
+    throw new Meteor.Error(403, 'unauthorized');
   }
 
   return Stylists.find({ owner: this.userId });
-});
-
-Meteor.publish('stylists', function stylists(owner) {
-  check(owner, String);
-
-  if (!this.userId) {
-    return null;
-  }
-
-  return Stylists.find({ owner });
 });
