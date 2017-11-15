5f145f22fba84029de0007a23178b9617a656eee
Chris Chen <chrischen79@gmail.com>
2017-11-15 23:06:27 +1100

    udpate addons publication to show public ones only

--- imports/api/addons/server/publications.js
+++ imports/api/addons/server/publications.js
@@ -3,11 +3,14 @@ import Addons from '../addons';
 
 Meteor.publish('addons', () =>
   Addons.find(
-    {},
+    { public: true },
     {
       // hide fields in the return
       fields: {
-        createdAt: 0, updatedAt: 0, createdBy: 0, public: 0,
+        createdAt: 0,
+        updatedAt: 0,
+        createdBy: 0,
+        public: 0,
       },
     },
   ));
