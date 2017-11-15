85bfa8c022c836f48bbde29bb3a4da2a3d384422
Chris Chen <chrischen79@gmail.com>
2017-11-15 22:41:11 +1100

    save user-defined addons

--- imports/api/stylists/server/methods.js
+++ imports/api/stylists/server/methods.js
@@ -2,9 +2,11 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { Roles } from 'meteor/alanning:roles';
 import log from 'winston';
+import _ from 'lodash';
 
 import rateLimit from '../../../modules/server/rate-limit';
 import Stylists from '../stylists';
+import Addons from '../../addons/addons';
 
 Meteor.methods({
   'stylists.update.services': function updateStylistsServices(services) {
@@ -12,12 +14,31 @@ Meteor.methods({
       throw new Meteor.Error(403, 'unauthorized');
     }
 
-    log.info('Meteor.methods - stylists.update.services', `userId - ${this.userId}`);
-
     check(services, Array);
 
     try {
       Stylists.update({ owner: this.userId }, { $set: { services } });
+
+      // insert user-defined addons
+      Meteor.defer(() => {
+        services.forEach((service) => {
+          const addonNames = service.addons.map(addon => addon.name);
+          const publicAddonNames = Addons.find({ serviceId: service._id })
+            .fetch()
+            .map(addon => addon.name);
+          const newAddonNames = _.differenceWith(addonNames, publicAddonNames, [
+            (arrVal, othVal) => _.isEqual(arrVal.toLowerCase(), othVal.toLowerCase()),
+          ]);
+
+          newAddonNames.forEach((name) => {
+            Addons.insert({
+              serviceId: service._id, name, createdBy: this.userId, public: false,
+            });
+          });
+        });
+      });
+
+      log.info('Meteor.methods - stylists.update.services', `userId - ${this.userId}`);
     } catch (exception) {
       /* eslint-disable no-console */
       console.error(exception);
