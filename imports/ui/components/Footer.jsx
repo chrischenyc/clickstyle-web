259d168d18530a2e30927131ec95dd3683f7f132
Chris Chen <chrischen79@gmail.com>
2017-12-19 23:47:37 +1100

    update footer

--- imports/ui/components/Footer.jsx
+++ imports/ui/components/Footer.jsx
@@ -2,61 +2,73 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { ServiceNameToSEOName } from '../../modules/seo-name';
+
 const Footer = () => {
   const {
     appName, facebookUrl, twitterUrl, instagramUrl, supportEmail,
   } = Meteor.settings.public;
 
+  const services = [
+    'Make Up',
+    'Hair - Women',
+    'Hair - Men',
+    'Hair - Children',
+    'Wedding',
+    'Nails',
+    'Eyebrow',
+    'Eyelash',
+    'Massage & Spa - Women',
+    'Massage & Spa - Men',
+    'Tanning',
+    'Waxing',
+    'Skincare treatment',
+  ];
+
   return (
     <div id="footer" className="dark">
       <div className="container">
         <div className="row">
-          <div className="col-md-5 col-sm-6">
-            <p>
-              TODO: blurb for SEO - Morbi convallis bibendum urna ut viverra. Maecenas quis
-              consequat libero, a feugiat eros. Nunc ut lacinia tortor morbi ultricies laoreet
-              ullamcorper phasellus semper.
-            </p>
-            <p>&copy; 2017 {appName}. All Rights Reserved.</p>
-          </div>
-
-          <div className="col-md-4 col-sm-6 ">
+          <div className="col-md-3 col-sm-6">
+            <h4>{Meteor.settings.public.appName}</h4>
             <ul className="footer-links">
               <li>
-                <Link to="/about">About Us</Link>
+                <Link to="/about">About</Link>
               </li>
               <li>
-                <Link to="/contact">Contact Us</Link>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/contact">FAQ</Link>
               </li>
               <li>
                 <Link to="/privacy">Privacy</Link>
               </li>
               <li>
-                <Link to="/terms">Terms &amp; Condition</Link>
+                <Link to="/terms">Terms</Link>
               </li>
-              <li>
-                <Link to="/sitemap">Sitemap</Link>
-              </li>
-            </ul>
-
-            <ul className="footer-links">
               <li>
                 <Link to="/join">Become a stylist</Link>
               </li>
-              <li>
-                <Link to="/how-it-works">How it works</Link>
-              </li>
-              <li>
-                <Link to="/help">Help</Link>
-              </li>
-              <li>
-                <Link to="/blog">Blog</Link>
-              </li>
             </ul>
+
             <div className="clearfix" />
           </div>
 
-          <div className="col-md-3  col-sm-12">
+          <div className="col-md-3 col-sm-6">
+            <h4>Browse services</h4>
+            <ul className="footer-links">
+              {services.map(service => (
+                <li key={service}>
+                  <Link to={`/stylists/${ServiceNameToSEOName(service)}`}>{service}</Link>
+                </li>
+              ))}
+            </ul>
+
+            <div className="clearfix" />
+          </div>
+
+          <div className="col-md-3 col-sm-12">
             <h4>Connect with us</h4>
             <div className="text-widget">
               <span>
@@ -82,6 +94,15 @@ const Footer = () => {
                 </a>
               </li>
             </ul>
+          </div>
+
+          <div className="col-md-3 col-sm-12">
+            <p>
+              TODO: blurb for SEO - Morbi convallis bibendum urna ut viverra. Maecenas quis
+              consequat libero, a feugiat eros. Nunc ut lacinia tortor morbi ultricies laoreet
+              ullamcorper phasellus semper.
+            </p>
+            <p>&copy; 2017 {appName}. All Rights Reserved.</p>
           </div>
         </div>
 
