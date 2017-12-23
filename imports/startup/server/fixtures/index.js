2f390963bdcf296abf4ca73baaad398070672eaf
Chris Chen <chrischen79@gmail.com>
2017-12-24 01:31:59 +1100

    undo one time off db fix

--- imports/startup/server/fixtures/index.js
+++ imports/startup/server/fixtures/index.js
@@ -3,14 +3,3 @@ import './services';
 import './suburbs';
 import './featured';
 // import './demo_stylists';
-
-// normalise Profiles.name -> Stylists.name
-import Stylists from '../../../api/stylists/stylists';
-import Profiles from '../../../api/profiles/profiles';
-
-const stylists = Stylists.find().fetch();
-stylists.forEach((stylist) => {
-  const { owner } = stylist;
-  const { name, photo, address } = Profiles.findOne({ owner });
-  Stylists.update({ owner }, { $set: { name, photo, address } });
-});
