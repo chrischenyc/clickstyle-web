67d7cc00f430926edce165c5d96593681bb26cfb
Chris Chen <chrischen79@gmail.com>
2018-01-10 00:18:23 +1100

    fix login page mobile version, missing padding

--- imports/ui/layouts/user/Login/LoginPage.jsx
+++ imports/ui/layouts/user/Login/LoginPage.jsx
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Form, Grid, Divider, Button } from 'semantic-ui-react';
+import { Form, Grid, Divider, Button, Container } from 'semantic-ui-react';
 import _ from 'lodash';
 
 import ModalLink from '../../../components/ModalLink';
@@ -15,74 +15,76 @@ import ForgotPassword from '../ForgotPassword/ForgotPassword';
 const LoginPage = ({
   onSubmit, onChange, loading, errors, modal, onLoggedIn,
 }) => (
-  <Grid textAlign="center" verticalAlign="middle" style={{ marginTop: modal ? '0' : '51px' }}>
-    <Grid.Row style={{ maxWidth: 450 }}>
-      <Grid.Column>
-        <SocialLoginButtons onLoggedIn={onLoggedIn} />
+  <Container className="margin-top-20">
+    <Grid textAlign="center">
+      <Grid.Row style={{ maxWidth: 450 }}>
+        <Grid.Column>
+          <SocialLoginButtons onLoggedIn={onLoggedIn} />
 
-        <Divider horizontal>or</Divider>
+          <Divider horizontal>or</Divider>
 
-        <Form onSubmit={onSubmit} loading={loading} error={!_.isEmpty(errors)}>
-          <FormInputField
-            fluid
-            icon="mail"
-            iconPosition="left"
-            placeholder="Email address"
-            type="email"
-            name="email"
-            size="huge"
-            onChange={onChange}
-            errors={errors}
-          />
+          <Form onSubmit={onSubmit} loading={loading} error={!_.isEmpty(errors)}>
+            <FormInputField
+              fluid
+              icon="mail"
+              iconPosition="left"
+              placeholder="Email address"
+              type="email"
+              name="email"
+              size="huge"
+              onChange={onChange}
+              errors={errors}
+            />
 
-          <FormInputField
-            fluid
-            icon="lock"
-            iconPosition="left"
-            placeholder="Password"
-            type="password"
-            name="password"
-            size="huge"
-            onChange={onChange}
-            errors={errors}
-          />
+            <FormInputField
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+              name="password"
+              size="huge"
+              onChange={onChange}
+              errors={errors}
+            />
 
-          <Button circular color="teal" fluid size="huge" type="submit">
-            Login
-          </Button>
+            <Button circular color="teal" fluid size="huge" type="submit">
+              Login
+            </Button>
 
-          {!_.isEmpty(errors.message) && (
-            <div className="notification error"> {errors.message} </div>
-          )}
-        </Form>
+            {!_.isEmpty(errors.message) && (
+              <div className="notification error"> {errors.message} </div>
+            )}
+          </Form>
 
-        {modal ? (
-          <ModalLink to="/forgot-password" component={<ForgotPassword modal />}>
-            <p className="margin-top-20 margin-bottom-20">Forgot password?</p>
-          </ModalLink>
-        ) : (
-          <Link to="/forgot-password">
-            <p className="margin-top-20 margin-bottom-20">Forgot password?</p>
-          </Link>
-        )}
-
-        <p className="margin-top-20 margin-bottom-20">
-          Don&apos;t have an account?&nbsp;
           {modal ? (
-            <ModalLink
-              to="/signup"
-              component={<SignUp modal onLoggedIn={onLoggedIn} />}
-              title="Join us"
-            >
-              Sign up here
+            <ModalLink to="/forgot-password" component={<ForgotPassword modal />}>
+              <p className="margin-top-20 margin-bottom-20">Forgot password?</p>
             </ModalLink>
           ) : (
-            <Link to="/signup">Sign up</Link>
+            <Link to="/forgot-password">
+              <p className="margin-top-20 margin-bottom-20">Forgot password?</p>
+            </Link>
           )}
-        </p>
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
+
+          <p className="margin-top-20 margin-bottom-20">
+            Don&apos;t have an account?&nbsp;
+            {modal ? (
+              <ModalLink
+                to="/signup"
+                component={<SignUp modal onLoggedIn={onLoggedIn} />}
+                title="Join us"
+              >
+                Sign up here
+              </ModalLink>
+            ) : (
+              <Link to="/signup">Sign up</Link>
+            )}
+          </p>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </Container>
 );
 
 LoginPage.defaultProps = {
--- imports/ui/layouts/user/SignUp/SignUpPage.jsx
+++ imports/ui/layouts/user/SignUp/SignUpPage.jsx
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Button, Form, Grid, Divider, Checkbox } from 'semantic-ui-react';
+import { Button, Form, Grid, Divider, Checkbox, Container } from 'semantic-ui-react';
 import _ from 'lodash';
 
 import ModalLink from '../../../components/ModalLink';
@@ -22,118 +22,120 @@ const SignUpPage = ({
   onLoggedIn,
   onDismissModal,
 }) => (
-  <Grid textAlign="center" verticalAlign="middle" style={{ marginTop: modal ? '0' : '51px' }}>
-    <Grid.Row style={{ maxWidth: 450 }}>
-      <Grid.Column>
-        <SocialLoginButtons isSignUp disabled={disabled} onLoggedIn={onLoggedIn} />
+  <Container className="margin-top-20">
+    <Grid textAlign="center">
+      <Grid.Row style={{ maxWidth: 450 }}>
+        <Grid.Column>
+          <SocialLoginButtons isSignUp disabled={disabled} onLoggedIn={onLoggedIn} />
 
-        <Divider horizontal>or</Divider>
+          <Divider horizontal>or</Divider>
 
-        <Form onSubmit={onSubmit} loading={loading} error={!_.isEmpty(errors)}>
-          <FormInputField
-            fluid
-            icon="mail"
-            iconPosition="left"
-            placeholder="Email address"
-            type="email"
-            name="email"
-            size="huge"
-            onChange={onChange}
-            errors={errors}
-          />
+          <Form onSubmit={onSubmit} loading={loading} error={!_.isEmpty(errors)}>
+            <FormInputField
+              fluid
+              icon="mail"
+              iconPosition="left"
+              placeholder="Email address"
+              type="email"
+              name="email"
+              size="huge"
+              onChange={onChange}
+              errors={errors}
+            />
 
-          <FormInputField
-            fluid
-            icon="user"
-            iconPosition="left"
-            placeholder="First name"
-            name="firstName"
-            size="huge"
-            onChange={onChange}
-            errors={errors}
-          />
+            <FormInputField
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="First name"
+              name="firstName"
+              size="huge"
+              onChange={onChange}
+              errors={errors}
+            />
 
-          <FormInputField
-            fluid
-            icon="user"
-            iconPosition="left"
-            placeholder="Last name"
-            name="lastName"
-            size="huge"
-            onChange={onChange}
-            errors={errors}
-          />
+            <FormInputField
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="Last name"
+              name="lastName"
+              size="huge"
+              onChange={onChange}
+              errors={errors}
+            />
 
-          <FormInputField
-            fluid
-            icon="lock"
-            iconPosition="left"
-            placeholder="Create a Password"
-            type="password"
-            name="password"
-            size="huge"
-            onChange={onChange}
-            errors={errors}
-          />
+            <FormInputField
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Create a Password"
+              type="password"
+              name="password"
+              size="huge"
+              onChange={onChange}
+              errors={errors}
+            />
 
-          <Button circular color="teal" fluid size="huge" type="submit" disabled={disabled}>
-            Sign up
-          </Button>
+            <Button circular color="teal" fluid size="huge" type="submit" disabled={disabled}>
+              Sign up
+            </Button>
 
-          {!_.isEmpty(errors.message) && (
-            <div className="notification error"> {errors.message} </div>
-          )}
-        </Form>
+            {!_.isEmpty(errors.message) && (
+              <div className="notification error"> {errors.message} </div>
+            )}
+          </Form>
 
-        <p className="margin-top-20">
-          Already have an account?&nbsp;
-          {modal ? (
-            <ModalLink
-              to="/login"
-              component={<Login modal onLoggedIn={onLoggedIn} />}
-              title="Log in to continue"
-            >
-              Log in here
-            </ModalLink>
-          ) : (
-            <Link to="/login">Log in</Link>
-          )}
-        </p>
+          <p className="margin-top-20">
+            Already have an account?&nbsp;
+            {modal ? (
+              <ModalLink
+                to="/login"
+                component={<Login modal onLoggedIn={onLoggedIn} />}
+                title="Log in to continue"
+              >
+                Log in here
+              </ModalLink>
+            ) : (
+              <Link to="/login">Log in</Link>
+            )}
+          </p>
 
-        <p className="margin-bottom-20">
-          <Checkbox
-            defaultChecked
-            onChange={onAgreement}
-            label={
-              <label htmlFor="agreement">
-                I confirm I am over 18 and I agree to {Meteor.settings.public.appName}&apos;s&nbsp;
-                <Link
-                  to="/terms"
-                  onClick={() => {
-                    if (modal && onDismissModal) {
-                      onDismissModal();
-                    }
-                  }}
-                >
-                  Terms of Use
-                </Link>&nbsp;and&nbsp;
-                <Link
-                  to="/privacy"
-                  onClick={() => {
-                    if (modal && onDismissModal) {
-                      onDismissModal();
-                    }
-                  }}
-                >
-                  Privacy Policy
-                </Link>.
-              </label>
-            }
-          />
-        </p>
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
+          <p className="margin-bottom-20">
+            <Checkbox
+              defaultChecked
+              onChange={onAgreement}
+              label={
+                <label htmlFor="agreement">
+                  I confirm I am over 18 and I agree to {Meteor.settings.public.appName}&apos;s&nbsp;
+                  <Link
+                    to="/terms"
+                    onClick={() => {
+                      if (modal && onDismissModal) {
+                        onDismissModal();
+                      }
+                    }}
+                  >
+                    Terms of Use
+                  </Link>&nbsp;and&nbsp;
+                  <Link
+                    to="/privacy"
+                    onClick={() => {
+                      if (modal && onDismissModal) {
+                        onDismissModal();
+                      }
+                    }}
+                  >
+                    Privacy Policy
+                  </Link>.
+                </label>
+              }
+            />
+          </p>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </Container>
 );
 
 SignUpPage.defaultProps = {
