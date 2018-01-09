0210e00263097df040fad0f7522c42e206bfc64a
Chris Chen <chrischen79@gmail.com>
2018-01-09 23:42:17 +1100

    add date and time picker to booking section

--- imports/ui/layouts/user-profile/StylistBookingSection.jsx
+++ imports/ui/layouts/user-profile/StylistBookingSection.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Button } from 'semantic-ui-react';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import { formatDate, parseDate } from 'react-day-picker/moment';
+import 'react-day-picker/lib/style.css';
+import '../../components/SearchBar/react-day-picker-custom.css';
+
+import TimeInput from '../../components/TimeInput';
+import {
+  formatDateQueryString,
+  parseDateQueryString,
+  formatDateDisplayString,
+  datePickerFormat,
+} from '../../../modules/format-date';
 
 import CartSummary from '../../components/CartSummary';
 
@@ -13,11 +25,37 @@ const StylistBookingSection = props => (
 
     <div className="row">
       <div className="col-lg-6 col-md-12">
-        <input type="text" id="booking-date" />
+        <DayPickerInput
+          // value={
+          //   parseDateQueryString(this.state.date).isValid()
+          //     ? formatDateDisplayString(parseDateQueryString(this.state.date))
+          //     : ''
+          // }
+          placeholder="Date"
+          onDayChange={(date) => {
+            // this.setState({ date: formatDateQueryString(date) });
+          }}
+          format={datePickerFormat}
+          formatDate={formatDate}
+          parseDate={parseDate}
+          dayPickerProps={{
+            modifiers: {
+              disabled: { before: new Date() },
+              // selected: day => formatDateQueryString(day) === this.state.date,
+            },
+          }}
+        />
       </div>
 
       <div className="col-lg-6 col-md-12">
-        <input type="text" id="booking-time" />
+        <TimeInput
+          placeholder="Time"
+          optional
+          // value={this.state.time}
+          onChange={(time) => {
+            // this.setState({ time });
+          }}
+        />
       </div>
     </div>
 
