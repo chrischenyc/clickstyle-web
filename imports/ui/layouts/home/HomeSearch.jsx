57b63e0833dc6e969900300da3ea7a8412891bb4
Chris Chen <chrischen79@gmail.com>
2018-01-11 15:23:49 +1100

    expand home animated keywords

--- imports/ui/layouts/home/HomeSearch.jsx
+++ imports/ui/layouts/home/HomeSearch.jsx
@@ -3,7 +3,74 @@ import Typist from 'react-typist';
 
 import SearchBar from '../../components/SearchBar/SearchBar';
 
-const animatedKeywords = ['hair stylist', 'makeup artist', 'wedding'];
+// TODO: prioritise keywords sorting
+const animatedKeywords = [
+  'Airbrush Makeup',
+  'Special Effects Makeup',
+  'Prom Makeup',
+  'Full Face Makeup',
+  'Cut & Style',
+  'Hair Updo',
+  'Shampoo & Style',
+  'Braids',
+  'Full Hair Color',
+  'Color Retouch',
+  'Full Highlights',
+  'Partial Highlights',
+  'Permanent Hair Treatment',
+  'Roller Set',
+  'Silk Press',
+  'Blowout',
+  'Hair Extension',
+  'Keratin Treatment',
+  'Hair Styling',
+  'Blowout with Iron',
+  'Curling',
+  'Beard Trim',
+  'Mens Haircut',
+  'Hot Towel Massage',
+  'Cut & Side design',
+  'Children Hair Cut',
+  'Bridal Makeup',
+  'Bridal Wedding Package',
+  'Bridal Makeup & Hair Styling',
+  'Bridal Hair Styling',
+  'Bridesmaid Hair Styling',
+  'Bridesmaid Makeup',
+  'Bridal Trials',
+  'Full Set Acrylic',
+  'Refill Acrylic Nails',
+  'Gel Manicure',
+  'Pedicure',
+  'Nail Art',
+  'Spa Pedicure',
+  'Manicure',
+  'Eyebrow Threading',
+  'Eyebrow Tinting',
+  'Eyebrow Shaping',
+  'Waxing,Lashes',
+  'Bottom Lashes',
+  'Mink Lashes',
+  'Eyelash Extension',
+  'Massage',
+  'Foot Massage',
+  'Hot Stone Massage',
+  'Aromatherapy Massage',
+  'Remedial Massage',
+  'Prenatal Massage',
+  'Full Body Airbrush Tan',
+  'Half Body Airbrush Tan',
+  'Brazilian',
+  'Bikini Line',
+  'Full Arm Wax',
+  'Full Leg Wax',
+  'Full Face Wax',
+  'Half Arm Wax',
+  'Half Leg Wax',
+  'Underarm Wax',
+  'Facial',
+  'Acne Facial',
+];
 
 const HomeSearch = () => (
   <div
@@ -21,7 +88,7 @@ const HomeSearch = () => (
                 {animatedKeywords.map(keyword => (
                   <span key={keyword}>
                     {keyword}
-                    <Typist.Backspace count={keyword.length} delay={keyword.length * 50} />
+                    <Typist.Backspace count={keyword.length} delay={keyword.length * 40} />
                   </span>
                 ))}
                 <span>my style!</span>
