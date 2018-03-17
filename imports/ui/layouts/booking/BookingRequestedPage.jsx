c9ce1ce40e189085a389fcda0380b445938eb430
Chris Chen <chrischen79@gmail.com>
2018-03-17 23:28:42 +1100

    update booking requested page copy and layout

--- imports/ui/layouts/booking/BookingRequestedPage.jsx
+++ imports/ui/layouts/booking/BookingRequestedPage.jsx
@@ -1,24 +1,28 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Container } from 'semantic-ui-react';
 
 const BookingRequestedPage = props => (
-  <div className="container">
-    <div className="row">
-      <div className="col-md-12">
-        <div className="booking-confirmation-page">
-          <i className="fa fa-check-circle" />
-          <h2 className="margin-top-30">Thanks for your booking!</h2>
-          <p>Your booking number is {props.booking._id}.</p>
-          <p>You&apos;ll receive a confirmation email.</p>
-          <p>TODO: copy writing what's next</p>
-          <Link to="/help" className="button margin-top-30">
-            FAQ
-          </Link>
-        </div>
-      </div>
+  <Container text>
+    <div className="booking-confirmation-page">
+      <i className="fa fa-check-circle" />
+      <h2 className="margin-top-30">
+        Thanks heaps for booking with {Meteor.settings.public.appName}!
+      </h2>
+      <p>Your booking number is {props.booking._id}.</p>
+      <p>
+        The team at {Meteor.settings.public.appName} will shortly send you an email confirmation. If
+        you have any queries in the meantime, then please don&apos;t hesitate to{' '}
+        <Link to="/contact">contact us</Link>.
+      </p>
+
+      <Link to="/help" className="button margin-top-30">
+        Frequently Asked Questions
+      </Link>
     </div>
-  </div>
+  </Container>
 );
 
 BookingRequestedPage.propTypes = {
