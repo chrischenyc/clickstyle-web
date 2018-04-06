457a2a9f005ff8393fb7283d96da37c3bbb1c1bc
Chris Chen <chrischen79@gmail.com>
2018-04-06 19:59:27 +1000

    add weekday to datetime formate

--- imports/modules/format-date.js
+++ imports/modules/format-date.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 export const dateString = date => moment(date).format('DD MMM YYYY');
-export const dateTimeString = date => moment(date).format('DD MMM YYYY, HH:mm');
+export const dateTimeString = date => moment(date).format('DD MMM YYYY (dddd), HH:mm');
 export const yearString = date => moment(date).format('YYYY');
 export const monthYearString = date => moment(date).format('MMMM YYYY');
 export const timestampString = date => moment(date).format('YYYYMMDDHHmmss');
