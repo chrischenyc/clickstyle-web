d12bda397b0e8d163acb0f7b0fe578573db170e9
Chris Chen <chrischen79@gmail.com>
2018-04-11 15:50:15 +1000

    add CFA contents on 404 page

--- imports/ui/layouts/NotFoundPage.jsx
+++ imports/ui/layouts/NotFoundPage.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
 import SearchBar from '../components/SearchBar/SearchBar';
+import HomeHowItWorks from '../layouts/home/HomeHowItWorks';
 
 const NotFoundPage = () => (
-  <div className="container">
+  <div className="container margin-bottom-50">
     <div className="row">
       <div className="col-md-12">
         <section id="not-found" className="center">
           <p>We&apos;re sorry, but the page you were looking for doesn&apos;t exist.</p>
+
+          <p>You may try to search what you like</p>
+
           <SearchBar />
         </section>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-md-12">
+        <HomeHowItWorks />
       </div>
     </div>
   </div>
