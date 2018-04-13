19f3ae55f35046b216c928a81b305b27c0db7dcc
Chris Chen <chrischen79@gmail.com>
2018-04-14 00:05:41 +1000

    fix missing fields in profile publication

--- imports/api/profiles/server/publications.js
+++ imports/api/profiles/server/publications.js
@@ -10,11 +10,15 @@ Meteor.publish('profiles.self', function profilesOwner() {
     { owner: this.userId },
     {
       fields: {
+        email: 1,
         name: 1,
         mobile: 1,
         address: 1,
         photo: 1,
         notifications: 1,
+        stripeDefaultCardId: 1,
+        stripeDefaultCardLast4: 1,
+        stripeDefaultCardName: 1,
       },
     },
   );
