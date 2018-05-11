75751db195198683e75e407346eacb0bfa26eb54
Chris Chen <chrischen79@gmail.com>
2018-05-11 22:30:48 +1000

    fix email verify page style

--- imports/ui/layouts/user/VerifyEmailPage.jsx
+++ imports/ui/layouts/user/VerifyEmailPage.jsx
@@ -29,20 +29,20 @@ class VerifyEmailPage extends React.Component {
   renderMessage() {
     if (this.state.loading) {
       return (
-        <Message size="huge" warning>
+        <Message size="huge" warning style={{ marginTop: '6em', marginBottom: '6em' }}>
           Just one second, we are verifying your email..
         </Message>
       );
     } else if (this.state.error) {
       return (
-        <Message size="huge" error>
+        <Message size="huge" error style={{ marginTop: '6em', marginBottom: '6em' }}>
           {this.state.error}
         </Message>
       );
     }
 
     return (
-      <Message size="huge" success>
+      <Message size="huge" success style={{ marginTop: '6em', marginBottom: '6em' }}>
         All set, thank!
       </Message>
     );
