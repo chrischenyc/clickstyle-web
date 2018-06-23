a31d4af31453081e577dd51a3a79d57f9635188d
Chris Chen <chrischen79@gmail.com>
2018-06-23 21:04:25 +1000

    update home join banner bg image

--- imports/modules/format-date.js
+++ imports/modules/format-date.js
@@ -1,6 +1,5 @@
 import moment from 'moment';
 
-// needs timezone
 export const dateString = date => moment(date).format('DD MMM YYYY');
 export const dateTimeString = date => moment(date).format('DD MMM YYYY (ddd) HH:mm');
 export const yearString = date => moment(date).format('YYYY');
